ceafcbaff27a2389abd1b7e88515ada867a7f508
Michael <micflan@gmail.com>
2012-01-02 02:23:58 +0000

    Adding the location indicator back in for the new links wqq

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ $(document).ready(function(){
 	$('a span.new-wrapper').hide();
 
 	// Replace the 'Discover' link with something useful (the 'Lists' page is my choice)
-	$('.topics').html('<a href="/#!/lists" data-dynamite-selected="true">Lists</a>');
+	$('.topics').replaceWith('<li><a href="/#!/lists" class="cuslink">Lists</a></li>');
 
 	// Add another few links for good measure.
 	// You'd probably want to update these to suit yourself
-	$('#global-actions').append('<li><a class="js-hover" href="/#!/micflan/listen">Listen</a></li>');
-	$('#global-actions').append('<li><a class="js-hover" href="/#!/micflan/inishowen">Inishowen</a></li>');
+	$('#global-actions').append('<li><a class="cuslink js-hover" href="/#!/micflan/listen">Listen</a></li>');
+	$('#global-actions').append('<li><a class="cuslink js-hover" href="/#!/micflan/inishowen">Inishowen</a></li>');
 
-	// I tried to add a link to the DM popup by copying the <li..> from the dropdown-menu, but it didn't work...
-	// $('#global-actions').append('<li class="messages" data-name="messages"><a class="js-dm-dialog" href="/#!/" data-dynamite-selected="true"><span class="js-direct-message-count count"></span>DMs</a></li>');
 
+	// Adding the location indicator back in (change of background on selected 'tab').
+	$('.nav li').click(function(){
+		$(this).siblings().removeClass('active');
+	});
+
+	$('.cuslink').click(function(){
+		$(this).parent().addClass('active');
+	});
 });
